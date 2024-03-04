8f6a8d8a8c4e02acbd9511ae4b1ea727650b15b2
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2024-03-04 18:57:18 -0300

    fix: reset form after submit

--- src/pages/multi-step-form/index.tsx
+++ src/pages/multi-step-form/index.tsx
@@ -90,7 +90,10 @@ export default function MultiStepForm() {
           <pre>{JSON.stringify({ name, email, age }, null, 2)}</pre>
 
           <button
-            onClick={() => setStep(0)}
+            onClick={() => {
+              useMultiStepFormStore.setState({ name: "", email: "", age: 0 });
+              setStep(0);
+            }}
             className="mt-6 flex justify-center items-center hover:bg-blue-700 border border-blue-700 text-blue-700 hover:text-white px-4 py-2 rounded-md active:bg-blue-800 font-medium"
           >
             Reset
