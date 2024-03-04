226a8b67deb549a1be50baae6207de3e67c2a606
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2024-03-04 13:02:13 -0300

    refactor: search component

---
+++ src/pages/home/components/search.tsx
@@ -0,0 +1,91 @@
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { FaSearch, FaShare } from "react-icons/fa";
+import { MdClear } from "react-icons/md";
+import { useSearchParams } from "react-router-dom";
+
+interface FormValues {
+  search: string;
+}
+
+export const Search = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const querySearch = searchParams.get("q") ?? "";
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    values: {
+      search: querySearch,
+    },
+  });
+
+  function applyFilter(data: FormValues): void {
+    setSearchParams((state) => {
+      state.set("q", data.search);
+      return state;
+    });
+  }
+
+  function removeFilter(): void {
+    setSearchParams((state) => {
+      state.delete("q");
+      return state;
+    });
+  }
+
+  function shareResults(): void {
+    const url = window.location.href;
+
+    navigator.clipboard.writeText(url).then(() => {
+      toast.success("Results copied to clipboard");
+    });
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(applyFilter)}
+      className="flex items-center mb-4"
+    >
+      <div className="relative flex items-center">
+        <input
+          type="text"
+          {...register("search")}
+          className="border border-gray-300 rounded-md p-2 pe-7"
+          placeholder="Search for a repo"
+        />
+        {querySearch && (
+          <button
+            type="button"
+            onClick={removeFilter}
+            className="absolute right-0 text-red-700 rounded-md h-[25px] w-[25px] me-1"
+          >
+            <MdClear className="w-[25px] h-[25px]" />
+          </button>
+        )}
+      </div>
+
+      <button
+        type="submit"
+        className="flex justify-center items-center ms-2 hover:bg-blue-700 border border-blue-700 text-blue-700 hover:text-white px-4 py-2 rounded-md active:bg-blue-800 font-medium"
+      >
+        Search
+        <FaSearch className="ms-2" />
+      </button>
+
+      {querySearch && (
+        <button
+          type="button"
+          onClick={shareResults}
+          className="flex justify-center items-center ms-2 hover:bg-green-700 border border-green-700 text-green-700 hover:text-white px-4 py-2 rounded-md active:bg-green-800 font-medium"
+        >
+          Share results
+          <FaShare className="ms-2" />
+        </button>
+      )}
+    </form>
+  );
+};
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import Layout from "../../components/Layout";
 import { Repos } from "./components/repos";
+import { Search } from "./components/search";
 
 export default function Home() {
   return (
@@ -7,6 +8,8 @@ export default function Home() {
       <h1 className="text-3xl text-center text-blue-700 font-bold mb-10">
         <strong>gbLw1</strong> Github repos
       </h1>
+
+      <Search />
 
       <Repos />
     </Layout>
