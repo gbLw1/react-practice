6b10a9d8923fbaf083c2717c5f1514a8dcfae210
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2024-03-04 12:51:57 -0300

    chore: actions search

--- README.md
+++ README.md
@@ -6,9 +6,11 @@ simple react app with good practices
 
 - [x] Vite + React + TypeScript
 - [x] Routing with [React Router](https://reactrouter.com/en/main)
+- [x] Styling with [Tailwind CSS](https://tailwindcss.com/) + [Flowbite React](https://www.flowbite-react.com/)
 - [x] HTTP requests with [Axios](https://axios-http.com/)
 - [ ] HTTP State management with [React Query](https://tanstack.com/query/latest)
 - [ ] Form management & validation with [React Hook Form](https://react-hook-form.com/) + [Zod](https://zod.dev/)
-- [x] Styling with [Tailwind CSS](https://tailwindcss.com/) + [Flowbite React](https://www.flowbite-react.com/)
-- [ ] URL state management with query params
+- [x] URL state management with query params
+- [ ] Private routes
+- [ ] Social login authentication
 
--- src/pages/home/components/repos.tsx
+++ src/pages/home/components/repos.tsx
@@ -2,10 +2,43 @@ import { useEffect, useState } from "react";
 import { getUserRepos } from "../../../services/repos.service";
 import { Table } from "flowbite-react";
 import RepoModel from "../../../interfaces/repo.model";
+import { useForm } from "react-hook-form";
+import { useSearchParams } from "react-router-dom";
+import { FaSearch, FaShare } from "react-icons/fa";
+import { MdClear } from "react-icons/md";
+import toast, { Toaster } from "react-hot-toast";
+
+interface FormValues {
+  search: string;
+}
 
 export const Repos = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [repos, setRepos] = useState<RepoModel[]>([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const querySearch = searchParams.get("q") ?? "";
+
+  const {
+    register,
+    getValues,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    values: {
+      search: querySearch,
+    },
+  });
+
+  const filteredRepos = repos.filter(
+    (repo) =>
+      repo.name
+        .toLowerCase()
+        .includes(getValues("search").toLowerCase().trim()) ||
+      repo.description
+        ?.toLowerCase()
+        .includes(getValues("search").toLowerCase().trim()),
+  );
 
   async function getRepos(): Promise<void> {
     setLoading(true);
@@ -23,54 +56,120 @@ export const Repos = () => {
       .finally(() => setLoading(false));
   }
 
+  function applyFilter(data: FormValues): void {
+    setSearchParams((state) => {
+      state.set("q", data.search);
+      return state;
+    });
+  }
+
+  function removeFilter(): void {
+    setSearchParams((state) => {
+      state.delete("q");
+      return state;
+    });
+  }
+
+  function shareResults(): void {
+    const url = window.location.href;
+
+    navigator.clipboard.writeText(url).then(() => {
+      toast.success("Results copied to clipboard");
+    });
+  }
+
   useEffect(() => {
     getRepos();
   }, []);
 
   return (
-    <Table hoverable>
-      <Table.Head>
-        <Table.HeadCell>Name</Table.HeadCell>
-        <Table.HeadCell>Description</Table.HeadCell>
-        <Table.HeadCell>Stars</Table.HeadCell>
-        <Table.HeadCell>Action</Table.HeadCell>
-      </Table.Head>
+    <>
+      <Toaster position="top-right" />
 
-      {!loading && repos.length > 0 ? (
-        repos.map((repo) => (
-          <Table.Body className="divide-y">
-            <Table.Row className="dark:border-gray-700">
-              <Table.Cell className="whitespace-nowrap font-medium">
-                {repo.name}
-              </Table.Cell>
-              <Table.Cell>
-                {repo.description ?? (
-                  <p className="italic">No description</p>
-                )}
-              </Table.Cell>
-              <Table.Cell>{repo.stargazers_count}</Table.Cell>
-              <Table.Cell>
-                <a
-                  className="btn btn-sm btn-primary w-full sm:w-auto hover:text-blue-700"
-                  href={repo.html_url}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  View
-                </a>
+      <form
+        onSubmit={handleSubmit(applyFilter)}
+        className="flex items-center mb-4"
+      >
+        <div className="relative flex items-center">
+          <input
+            type="text"
+            {...register("search")}
+            className="border border-gray-300 rounded-md p-2 pe-7"
+            placeholder="Search for a repo"
+          />
+          {querySearch && (
+            <button
+              type="button"
+              onClick={removeFilter}
+              className="absolute right-0 text-red-700 rounded-md h-[25px] w-[25px] me-1"
+            >
+              <MdClear className="w-[25px] h-[25px]" />
+            </button>
+          )}
+        </div>
+
+        <button
+          type="submit"
+          className="flex justify-center items-center ms-2 hover:bg-blue-700 border border-blue-700 text-blue-700 hover:text-white px-4 py-2 rounded-md active:bg-blue-800 font-medium"
+        >
+          Search
+          <FaSearch className="ms-2" />
+        </button>
+
+        {querySearch && filteredRepos.length > 0 && (
+          <button
+            type="button"
+            onClick={shareResults}
+            className="flex justify-center items-center ms-2 hover:bg-green-700 border border-green-700 text-green-700 hover:text-white px-4 py-2 rounded-md active:bg-green-800 font-medium"
+          >
+            Share results
+            <FaShare className="ms-2" />
+          </button>
+        )}
+      </form>
+
+      <Table hoverable>
+        <Table.Head>
+          <Table.HeadCell>Name</Table.HeadCell>
+          <Table.HeadCell>Description</Table.HeadCell>
+          <Table.HeadCell>Stars</Table.HeadCell>
+          <Table.HeadCell>Action</Table.HeadCell>
+        </Table.Head>
+
+        {!loading && filteredRepos.length > 0 ? (
+          filteredRepos.map((repo) => (
+            <Table.Body className="divide-y">
+              <Table.Row className="dark:border-gray-700">
+                <Table.Cell className="whitespace-nowrap font-medium">
+                  {repo.name}
+                </Table.Cell>
+                <Table.Cell>
+                  {repo.description ?? <p className="italic">No description</p>}
+                </Table.Cell>
+                <Table.Cell>{repo.stargazers_count}</Table.Cell>
+                <Table.Cell>
+                  <a
+                    className="btn btn-sm btn-primary w-full sm:w-auto hover:text-blue-700"
+                    href={repo.html_url}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    View
+                  </a>
+                </Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          ))
+        ) : (
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell colSpan={5} className="text-center">
+                {loading ? "Loading..." : "No repos found"}
               </Table.Cell>
             </Table.Row>
           </Table.Body>
-        ))
-      ) : (
-        <Table.Body>
-          <Table.Row>
-            <Table.Cell colSpan={5} className="text-center">
-              {loading ? "Loading..." : "No repos found"}
-            </Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      )}
-    </Table>
+        )}
+      </Table>
+    </>
   );
 };
