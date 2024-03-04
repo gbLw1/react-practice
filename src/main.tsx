22b5bf1e1bd6a93d8ef0d185cd681d9d3223deb6
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2024-03-04 15:25:21 -0300

    fix: toaster

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import "./index.css";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />,
+  <>
+    <Toaster position="top-right" />
+    <RouterProvider router={router} />
+  </>,
 );
