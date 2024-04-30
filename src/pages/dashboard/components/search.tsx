4c0e78148b9795e4398b52c0180eb9a0b50377ed
Gabriel Grassi <gabrielhenrique2504@gmail.com>
2024-04-30 17:55:55 -0300

    minimalist search icons

--- src/pages/dashboard/components/search.tsx
+++ src/pages/dashboard/components/search.tsx
@@ -6,8 +6,14 @@ import { MdClear } from "react-icons/md";
 import { useSearchParams } from "react-router-dom";
 import { z } from "zod";
 
+const searchRequiredMessage = "Search query is required";
+
 const searchSchema = z.object({
-  search: z.string().nonempty("Please enter a search term"),
+  search: z
+    .string({
+      required_error: searchRequiredMessage,
+    })
+    .min(1, searchRequiredMessage),
 });
 
 type FormValues = z.infer<typeof searchSchema>;
@@ -46,7 +52,7 @@ export const Search = () => {
     const url = window.location.href;
 
     navigator.clipboard.writeText(url).then(() => {
-      toast.success("Search results link copied to clipboard");
+      toast.success("Link to search results copied to clipboard");
     });
   }
 
@@ -60,14 +66,14 @@ export const Search = () => {
           <input
             type="text"
             {...register("search")}
-            className="border border-gray-300 rounded-md p-2 pe-7"
+            className="p-2 block w-full border border-slate-500 rounded-md h-12 bg-slate-800 text-white"
             placeholder="Search for a repo"
           />
           {querySearch && (
             <button
               type="button"
               onClick={removeFilter}
-              className="absolute right-0 text-red-700 rounded-md h-[25px] w-[25px] me-1"
+              className="absolute top-3 right-0 text-red-700 rounded-md h-[25px] w-[25px] me-1"
             >
               <MdClear className="w-[25px] h-[25px]" />
             </button>
@@ -81,20 +87,18 @@ export const Search = () => {
 
       <button
         type="submit"
-        className="flex justify-center items-center ms-2 hover:bg-blue-700 border border-blue-700 text-blue-700 hover:text-white px-4 py-2 rounded-md active:bg-blue-800 font-medium"
+        className="h-12 flex justify-center items-center ms-2 hover:bg-slate-700 border border-slate-400 text-slate-400 hover:text-white px-4 py-2 rounded-md active:bg-slate-800 font-medium"
       >
-        Search
-        <FaSearch className="ms-2" />
+        <FaSearch />
       </button>
 
       {querySearch && (
         <button
           type="button"
           onClick={shareResults}
-          className="flex justify-center items-center ms-2 hover:bg-green-700 border border-green-700 text-green-700 hover:text-white px-4 py-2 rounded-md active:bg-green-800 font-medium"
+          className="h-12 flex justify-center items-center ms-2 hover:bg-green-700 border border-green-700 text-green-700 hover:text-white px-4 py-2 rounded-md active:bg-green-800 font-medium"
         >
-          Share results
-          <FaShare className="ms-2" />
+          <FaShare />
         </button>
       )}
     </form>
